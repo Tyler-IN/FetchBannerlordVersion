1883062ea5fdfdbf43d9aa33cdb3a6af944d6801
Vitalii Mikhailov <vitalii.mikhailov@amdaris.com>
2023-01-23 14:43:52 +0200

    Fix

--- src/FetchBannerlordVersion.Native.TypeScript/src/test/FetchBannerlordVersion.spec.ts
+++ src/FetchBannerlordVersion.Native.TypeScript/src/test/FetchBannerlordVersion.spec.ts
@@ -6,23 +6,12 @@ test('sort', async (t) => {
   const path = __dirname;
   const dllName = 'TaleWorlds.Library.dll';
 
+  const changeSet = FetchBannerlordVersion.getChangeSet(path, dllName);
+  t.is(changeSet, 321460);
+
   const version = FetchBannerlordVersion.getVersion(path, dllName);
-  if (version !== 'e1.8.0') {
-    t.fail();
-    return;
-  }
+  t.is(version, 'e1.8.0');
 
   const versionType = FetchBannerlordVersion.getVersionType(path, dllName);
-  if (versionType !== 4) {
-    t.fail();
-    return;
-  }
-
-  const changeSet = FetchBannerlordVersion.getChangeSet(path, dllName);
-  if (changeSet !== 321460) {
-    t.fail();
-    return;
-  }
-
-  t.pass();
+  t.is(versionType, 4);
 });
